953cf0570fcbfcf089ab3a377ec4f5e152741732
JCPedroza <nemesis4go10@hotmail.com>
2022-12-21 06:56:40 -0600

    feat: implement more assertions

--- tests/assert.ts
+++ tests/assert.ts
@@ -2,22 +2,33 @@
  * Assertion utilities.
  */
 
-interface EqualityAssertion {
-  toEqual: (numsB: number[]) => void
+interface Range {
+  from: number
+  to: number
+}
+
+interface AssertionAggregate {
+  toEqual: (numsB: number[]) => AssertionAggregate
+  toHaveLength: (length: number) => AssertionAggregate
+  toHaveIntegersOnly: () => AssertionAggregate
+  toHaveNumbersOnly: () => AssertionAggregate
+  toHaveValuesInRange: (range: Range) => AssertionAggregate
+  toBeInAscendingOrder: () => AssertionAggregate
+  toBeInDescendingOrder: () => AssertionAggregate
 }
 
 interface Expectation {
-  expect: (numsA: number[]) => EqualityAssertion
+  expect: (numsA: number[]) => AssertionAggregate
 }
 
 const throwError = (
   why: string,
   id: string,
   numsA: number[],
-  numsB: number[]
+  numsB?: any
 ): void => {
   const strNumsA = String(numsA)
-  const strNumsB = String(numsB)
+  const strNumsB = (numsB !== undefined && numsB !== null) ? String(numsB) : ''
   const msg = `${why}\n${id}\nReturned: [${strNumsA}]\nExpected: [${strNumsB}]`
   throw new Error(msg)
 }
@@ -33,8 +44,8 @@ export const buildId =
  * @returns An Expectation used for assersions.
  */
 export const test = (id: string): Expectation => ({
-  expect: (numsA: number[]): EqualityAssertion => ({
-    toEqual: (numsB: number[]): void => {
+  expect: (numsA: number[]): AssertionAggregate => ({
+    toEqual: (numsB: number[]): AssertionAggregate => {
       if (numsA.length !== numsB.length) { // Arrays of different length are not equal
         throwError('Length difference', id, numsA, numsB)
       }
@@ -44,6 +55,60 @@ export const test = (id: string): Expectation => ({
           throwError('Inequality', id, numsA, numsB)
         }
       }
+
+      return test(id).expect(numsA)
+    },
+
+    toHaveLength: (length: number): AssertionAggregate => {
+      if (numsA.length !== length) {
+        throwError('Unexpected length', id, numsA, length)
+      }
+
+      return test(id).expect(numsA)
+    },
+
+    toHaveIntegersOnly: (): AssertionAggregate => {
+      if (!numsA.every(num => Number.isInteger(num))) {
+        throwError('Non-integer value', id, numsA)
+      }
+
+      return test(id).expect(numsA)
+    },
+
+    toHaveNumbersOnly: (): AssertionAggregate => {
+      if (!numsA.every(num => typeof num === 'number')) {
+        throwError('Non-numeric value', id, numsA)
+      }
+
+      return test(id).expect(numsA)
+    },
+
+    toHaveValuesInRange: (range: Range): AssertionAggregate => {
+      if (!numsA.every(num => num >= range.from && num < range.to)) {
+        throwError('Value out of range', id, numsA, range)
+      }
+
+      return test(id).expect(numsA)
+    },
+
+    toBeInAscendingOrder: (): AssertionAggregate => {
+      for (let idx = 0; idx < numsA.length - 1; idx++) {
+        if (numsA[idx] > numsA[idx + 1]) {
+          throwError('Array not in ascending order', id, numsA)
+        }
+      }
+
+      return test(id).expect(numsA)
+    },
+
+    toBeInDescendingOrder: (): AssertionAggregate => {
+      for (let idx = 0; idx < numsA.length - 1; idx++) {
+        if (numsA[idx] < numsA[idx + 1]) {
+          throwError('Array not in descending order', id, numsA)
+        }
+      }
+
+      return test(id).expect(numsA)
     }
   })
 })
