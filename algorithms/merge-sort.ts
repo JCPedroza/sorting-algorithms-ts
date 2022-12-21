1f2448a11e766500104af01f9e86f1c20a63f59e
JCPedroza <nemesis4go10@hotmail.com>
2022-12-21 03:02:04 -0600

    feat: implement & test merge sort

---
+++ algorithms/merge-sort.ts
@@ -0,0 +1,37 @@
+/**
+ * Merge two numeric arrays by their first element, prioritizing the lowest value.
+ * If both arrays are sorted, the merged array will be sorted too (ascending).
+ * Complexity: time O(n), space O(n).
+ * 
+ * @param left Numeric array to be merged.
+ * @param right Numeric array to be merged.
+ * @returns The first-element low-value-priority merge of the two input arrays. 
+ */
+const merge = (left: number[], right: number[]) => {
+  const both: number[] = []
+
+  while (left.length && right.length) {
+    both.push(left[0] < right[0] ? left.shift() : right.shift())
+  }
+
+  return [...both, ...left, ...right]
+}
+
+/**
+ * Sorts numeric array, in ascending order, not-in-place, using Merge Sort.
+ * Complexity: time O(n log n), space O(n).
+ *
+ * @param nums Numeric array to be sorted.
+ * @returns Reference to the input array, now sorted.
+ */
+const mergeSort = (nums: number[]): number[] => {
+  if (nums.length < 2) return nums
+
+  const mid = nums.length / 2
+  const left = nums.slice(0, mid)
+  const right = nums.slice(mid, nums.length)
+
+  return merge(mergeSort(left), mergeSort(right))
+}
+
+export default mergeSort
--- algorithms/sort.repo.ts
+++ algorithms/sort.repo.ts
@@ -2,15 +2,17 @@ import bubbleSort from './bubble-sort'
 import selectionSort from './selection-sort'
 import insertionSwapSort from './insertion-swap-sort'
 import insertionShiftSort from './insertion-shift-sort'
+import mergeSort from './merge-sort'
 
 /**
- * Collection of sort functions.
+ * Collection of sort functions for use in testing, profiling, etc.
  */
 const sorters = [
   bubbleSort,
   selectionSort,
   insertionSwapSort,
-  insertionShiftSort
+  insertionShiftSort,
+  mergeSort
 ]
 
 export default sorters
