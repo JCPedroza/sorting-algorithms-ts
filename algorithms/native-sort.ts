14c6ec60f95236b80d3ad8b4ff0d5a3d9b90c5ed
JCPedroza <nemesis4go10@hotmail.com>
2022-12-22 00:25:33 -0600

    refa: add comments

--- algorithms/native-sort.ts
+++ algorithms/native-sort.ts
@@ -1,3 +1,11 @@
+/**
+ * Sorts numeric array, in ascending order, in-place, using the sort implementation
+ * provided by the runtime.
+ * Complexity: Depends on the runtime's sort implementation.
+ *
+ * @param nums Numeric array to be sorted.
+ * @returns Reference to the input array, now sorted.
+ */
 const nativeSort =
   (nums: number[]): number[] =>
     nums.sort((a, b) => a - b)
