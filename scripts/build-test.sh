6a4e86011959b0eefc750388860263c6753fbb56
JCPedroza <nemesis4go10@hotmail.com>
2022-12-22 03:26:15 -0600

    feat: add build test script

---
+++ scripts/build-test.sh
@@ -0,0 +1,18 @@
+# Transpile to JS, then delete directory containing transpiled JS
+
+if ! command -v node
+then
+    printf "\nThe 'node' command cannot be found.\n"
+    printf "You need NodeJS installed to run this script.\n\n"
+    exit
+fi
+
+if ! command -v npm
+then
+    printf "\nThe 'npm' command cannot be found.\n"
+    printf "You need NPM installed to run this script.\n\n"
+    exit
+fi
+
+time sh -c 'npm run build; rm -r ./dist'
+printf '\n'
