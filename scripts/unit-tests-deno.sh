0e8f11d07d6f86a321c88c428991a3642380ab3d
JCPedroza <nemesis4go10@hotmail.com>
2022-12-26 08:02:12 -0600

    refa: time using sh and -c

--- scripts/unit-tests-deno.sh
+++ scripts/unit-tests-deno.sh
@@ -1,10 +1,12 @@
+# Run unit tests with Deno
+
 if ! command -v deno
 then
-    printf "\nThe 'deno' command cannot be found.\n"
-    printf "You need Deno installed to run this script.\n\n"
-    exit
+  printf '\nThe 'deno' command cannot be found.\n'
+  printf 'You need Deno installed to run this script.\n\n'
+  exit
 fi
 
-printf "\n> deno ./tests/sort.test.ts\n\n"
-time deno ./tests/sort.test.ts
-printf "\n"
+printf '\n'
+time sh -c 'deno run ./tests/sort.test.ts && deno run ./tests/prof-utils.test.ts'
+printf '\n'
